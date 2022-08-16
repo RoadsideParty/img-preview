fb1a95269ccaf0ec72eeb575d5af0b100a358e8e
psp <976477689@qq.com>
2022-08-16 20:37:48 +0800

    success

--- preview.ts
+++ preview.ts
@@ -3,7 +3,7 @@ class Preview {
     scale: number
     size: number
     previewDom: HTMLDivElement
-    constructor(el: HTMLImageElement, scale: number = 1, size: number = 300) {
+    constructor(el: HTMLImageElement, scale: number = 3, size: number = 300) {
         this.el = el
         this.scale = scale
         this.size = size
@@ -14,8 +14,9 @@ class Preview {
         this.getMousePos()
     }
     createPreview() {
-        this.previewDom = document.createElement('div')
-        this.previewDom.style.cssText = `
+        const previewDom = document.createElement('div')
+        previewDom.style.cssText = `
+            display:none;
             position:fixed;
             width:${this.size}px;
             height:${this.size}px;
@@ -25,18 +26,39 @@ class Preview {
             background-image: url(${this.el.getAttribute('src')});
             background-repeat:no-repeat;
             background-position:0 0;
-            background-size:auto ${this.scale * 100}%;
+            background-size:${this.el.clientWidth * this.scale}px auto;
         `
-        document.body.append(this.previewDom)
+        document.body.append(previewDom)
+        this.previewDom = previewDom
     }
     getMousePos() {
-        this.el.addEventListener('mousemove', (e: MouseEvent) => {
+        window.addEventListener('mousemove', (e: MouseEvent) => {
             const { clientX, clientY } = e
             const { left, top } = this.el.getBoundingClientRect()
-            this.updatePreview((clientX - left) * 3, (clientY - top) * 3)
+            if (this.mouseInTarget(clientX, clientY, left, top)) {
+                this.previewDom.style.display = 'inline-block'
+                this.updatePreviewBox(clientX, clientY)
+                this.updatePreview((clientX - left), (clientY - top))
+            } else {
+                this.previewDom.style.display = 'none'
+            }
         })
     }
+    updatePreviewBox(x: number, y: number) {
+        if (x + this.size > window.innerWidth) {
+            this.previewDom.style.left = window.innerWidth - this.size + 'px'
+        } else {
+            this.previewDom.style.left = x + 'px'
+        }
+        this.previewDom.style.top = y + 'px'
+    }
     updatePreview(x: number, y: number) {
-        this.previewDom.style['background-position'] = `${-x + 150}px ${-y + 150}px`
+        this.previewDom.style['background-position'] = `${-x * this.scale + this.size / 2}px ${-y * this.scale + this.size / 2}px`
+    }
+    mouseInTarget(x: number, y: number, x1: number, y1: number) {
+        if (x < x1 || x > x1 + this.el.clientWidth || y < y1 || y > y1 + this.el.clientHeight) {
+            return false
+        }
+        return true
     }
 }
